e7152a7823c05b42ceb2a37bebaf35dcb9ab81b3
budhas <s.budha@elsevier.com>
2020-04-16 02:00:25 +0200

    Removing alerts and adding messages on Bingo Screens

--- pages/bingoGuest.js
+++ pages/bingoGuest.js
@@ -36,6 +36,7 @@ function runPop(value,className){
   }
 
 }
+
 function videoOperation(buttonName,url) {
   if(buttonName=="Hide"){
         document.getElementById("videoFrame").style.visibility = "hidden";
@@ -51,6 +52,10 @@ function videoOperation(buttonName,url) {
         document.getElementById("youtubeBtn").innerHTML="Hide"
   }
 
+}
+
+function clearMessage(){
+  document.getElementById("alertBox").innerHTML="";
 }
 
 const youtubeVideo = async (buttonName) => {
@@ -70,8 +75,47 @@ const youtubeVideo = async (buttonName) => {
                             username: username
                             }
                           });
-                          alert(responseToken.data.message);
-        };
+    if(responseToken.data.message.includes("Congratulations")){
+        document.getElementById("alertBox").className=document.getElementById("content").className+" alertDivStatus";
+      }else{
+        document.getElementById("alertBox").className=document.getElementById("content").className+" alertDiv";
+      }
+      document.getElementById("alertBox").innerHTML='<strong>Alert!</strong>'+responseToken.data.message;
+    };
+
+  const verifyNumberTaken = async (value,className) => {
+      var valueLatest= document.getElementById(value).innerHTML;
+      var newValue=0;
+
+      if(valueLatest.includes('.')) {
+        newValue= valueLatest.slice(0,valueLatest.length-1);
+      }else{
+        newValue=valueLatest;
+      }
+
+    const responseToken = await axios.get(`${API}/bingo-rest/bingo/verifyNumberIsTaken`,{
+                                params: {
+                                  takenNumber:newValue
+                                  }
+                                });
+
+     if(!valueLatest.includes('.') & responseToken.data==true) {
+       document.getElementById(value).innerHTML = valueLatest+'.';
+       document.getElementById(value).className=className.slice(0,className.length-1)+'3';
+       clearMessage();
+     }else{
+      if(responseToken.data==true){
+          document.getElementById(value).innerHTML = valueLatest.slice(0,valueLatest.length-1);
+          document.getElementById(value).className=className.slice(0,className.length-1)+'2';
+          clearMessage();
+      }else{
+        if(newValue>0){
+            document.getElementById("alertBox").className=document.getElementById("content").className+" alertDiv";
+            document.getElementById("alertBox").innerHTML='<strong>Alert!</strong> Sorry '+newValue+' is not yet Taken in Bingo Draw';
+        }
+      }
+     }
+  };
 
 const BingoGuest = ({bingoArray,username}) => (
 <Layout title="Home">
@@ -97,6 +141,10 @@ const BingoGuest = ({bingoArray,username}) => (
            <main>
              <div id="content">
                <button id="generateFreeBtn" onClick={() => newCard('generateFreeBtn',bingoArray)}>Get your Bingo Coupon</button>
+                <div class="alert">
+                  <span class="closebtn" onClick={() =>clearMessage()}>&times;</span>
+                  <div id="alertBox" className="alertDiv"></div>
+                </div>
                <table id="bingotable">
                <tbody>
                  <tr>
@@ -107,39 +155,39 @@ const BingoGuest = ({bingoArray,username}) => (
                    <td><button className="button button2" style={{background: '#9b59b6'}} onClick={() =>verifyResult(bingoArray,username,"column-O")}>O</button></td>
                  </tr>
                  <tr>
-                   <td> <button id="square0" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square0',document.getElementById('square0').className)}></button></td>
-                   <td> <button id="square5" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square5',document.getElementById('square5').className)}></button></td>
-                   <td> <button id="square10" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square10',document.getElementById('square10').className)}></button></td>
-                   <td> <button id="square15" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square15',document.getElementById('square15').className)}></button></td>
-                   <td> <button id="square20" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square20',document.getElementById('square20').className)}></button></td>
+                   <td> <button id="square0" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square0',document.getElementById('square0').className)}></button></td>
+                   <td> <button id="square5" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square5',document.getElementById('square5').className)}></button></td>
+                   <td> <button id="square10" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square10',document.getElementById('square10').className)}></button></td>
+                   <td> <button id="square15" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square15',document.getElementById('square15').className)}></button></td>
+                   <td> <button id="square20" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square20',document.getElementById('square20').className)}></button></td>
                  </tr>
                  <tr>
-                   <td> <button id="square1" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square1',document.getElementById('square1').className)}></button></td>
-                   <td> <button id="square6" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square6',document.getElementById('square6').className)}></button></td>
-                   <td> <button id="square11" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square11',document.getElementById('square11').className)}></button></td>
-                   <td> <button id="square16" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square16',document.getElementById('square16').className)}></button></td>
-                   <td> <button id="square21" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square21',document.getElementById('square21').className)}></button></td>
+                   <td> <button id="square1" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square1',document.getElementById('square1').className)}></button></td>
+                   <td> <button id="square6" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square6',document.getElementById('square6').className)}></button></td>
+                   <td> <button id="square11" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square11',document.getElementById('square11').className)}></button></td>
+                   <td> <button id="square16" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square16',document.getElementById('square16').className)}></button></td>
+                   <td> <button id="square21" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square21',document.getElementById('square21').className)}></button></td>
                  </tr>
                  <tr>
-                   <td> <button id="square2" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square2',document.getElementById('square2').className)}></button></td>
-                   <td> <button id="square7" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square7',document.getElementById('square7').className)}></button></td>
+                   <td> <button id="square2" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square2',document.getElementById('square2').className)}></button></td>
+                   <td> <button id="square7" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square7',document.getElementById('square7').className)}></button></td>
                    <td id="squarefree"> FREE</td>
-                   <td> <button id="square17" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square17',document.getElementById('square17').className)}></button></td>
-                   <td> <button id="square22" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square22',document.getElementById('square22').className)}></button></td>
+                   <td> <button id="square17" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square17',document.getElementById('square17').className)}></button></td>
+                   <td> <button id="square22" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square22',document.getElementById('square22').className)}></button></td>
                  </tr>
                  <tr>
-                   <td> <button id="square3" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square3',document.getElementById('square3').className)}></button></td>
-                   <td> <button id="square8" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square8',document.getElementById('square8').className)}></button></td>
-                   <td> <button id="square13" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square13',document.getElementById('square13').className)}></button></td>
-                   <td> <button id="square18" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square18',document.getElementById('square18').className)}></button></td>
-                   <td> <button id="square23" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square23',document.getElementById('square23').className)}></button></td>
+                   <td> <button id="square3" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square3',document.getElementById('square3').className)}></button></td>
+                   <td> <button id="square8" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square8',document.getElementById('square8').className)}></button></td>
+                   <td> <button id="square13" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square13',document.getElementById('square13').className)}></button></td>
+                   <td> <button id="square18" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square18',document.getElementById('square18').className)}></button></td>
+                   <td> <button id="square23" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square23',document.getElementById('square23').className)}></button></td>
                  </tr>
                  <tr>
-                   <td> <button id="square4" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square4',document.getElementById('square4').className)}></button></td>
-                   <td> <button id="square9" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square9',document.getElementById('square9').className)}></button></td>
-                   <td> <button id="square14" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square14',document.getElementById('square14').className)}></button></td>
-                   <td> <button id="square19" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square19',document.getElementById('square19').className)}></button></td>
-                   <td> <button id="square24" type="submit" className="button button2" value="&nbsp;" onClick={() => runPop('square24',document.getElementById('square24').className)}></button></td>
+                   <td> <button id="square4" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square4',document.getElementById('square4').className)}></button></td>
+                   <td> <button id="square9" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square9',document.getElementById('square9').className)}></button></td>
+                   <td> <button id="square14" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square14',document.getElementById('square14').className)}></button></td>
+                   <td> <button id="square19" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square19',document.getElementById('square19').className)}></button></td>
+                   <td> <button id="square24" type="submit" className="button button2" value="&nbsp;" onClick={() => verifyNumberTaken('square24',document.getElementById('square24').className)}></button></td>
                  </tr>
                 </tbody>
                </table>
@@ -154,6 +202,14 @@ const BingoGuest = ({bingoArray,username}) => (
                                                   justify-content: center;
                                                   align-items: center;
                                                 }
+                                       .alertDiv{
+                                            background: #f44336;
+                                            color:white;
+                                        }
+                                        .alertDivStatus{
+                                            background: #008000;
+                                            color:white;
+                                        }
 
                                                 h1, p {
                                                   font-family: 'Roboto', sans-serif; font-size: 20pt;
